2bb23e228bab9ac4fed8ca61073cdbb6abb44d60
skuhlmann <samkuhlmann@gmail.com>
2025-04-17 16:15:40 -0600

    more layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 export default function Home() {
   return (
-    <div className="p-20 bg-secondary">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="flex flex-col">
+      <div className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start p-20 bg-secondary flex-grow">
         <h1>title</h1>
         <p>words</p>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
+      </div>
     </div>
   );
 }
